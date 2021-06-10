9c097d754a97eef680c9813d1239d2e39f509f79
Abdullah0322 <abdullahnaveed71.am@gmail.com>
2021-06-10 20:01:33 +0500

    First commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>FA17-BSE-018, FA17-BSE-039, FA17-BSE-057  </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
