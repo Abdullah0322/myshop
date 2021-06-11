2c8de6203806877ef5d75358eed4dce7e8d9d00d
Abdullah0322 <abdullahnaveed71.am@gmail.com>
2021-06-11 16:17:58 +0500

    deploy takeof

--- backend/seeder.js
+++ backend/seeder.js
@@ -6,9 +6,9 @@ import products from './data/products.js'
 import User from './models/userModel.js'
 import Product from './models/productModel.js'
 import Order from './models/orderModel.js'
+dotenv.config()
 import connectDB from './config/db.js'
 
-dotenv.config()
 
 connectDB()
 
